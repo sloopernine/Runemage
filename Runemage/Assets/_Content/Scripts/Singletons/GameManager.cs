f563dbe46a4fff0d5ca693b34ba63618b705be17
sloopernine <robin@omnick.se>
2021-09-15 15:43:32 +0200

    Save training data to resource folder using button in inspector

---
+++ Runemage/Assets/_Content/Scripts/Singletons/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Singletons
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance;
+
+        public bool gestureTrainingMode;
+        
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(gameObject);
+        }
+    }
+}
